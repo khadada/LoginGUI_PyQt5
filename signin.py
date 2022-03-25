a4b6926526bd981008e9a7ca48d259802fddd5b5
khadada <lkhadada@gmail.com>
2022-03-25 17:51:46 +0100

    All done with all components without of functionality

--- signin.py
+++ signin.py
@@ -4,7 +4,13 @@ from PyQt5.QtWidgets import(QApplication, QWidget, QLabel, QPushButton, QLineEdi
 from PyQt5.QtGui import QPixmap #=> To work with icons
 from PyQt5.QtGui import QFont
 # our login gui
+# VARIABLES
+
 class LogInGUI(QWidget):
+    PRIMARY_COLOR = "#E48700"
+    SECONDARY_COLOR = "#4285F4"
+    BORDER_COLOR = "#E48700"
+    
     def __init__(self):
         super().__init__()
         self.initialize_ui()
@@ -30,7 +36,7 @@ class LogInGUI(QWidget):
         owner_projet_lbl = QLabel('Khaled GUI',self)
         owner_projet_lbl.setFont(QFont("Tahoma",16))
         owner_projet_lbl.move(200, 44)
-        owner_projet_lbl.setStyleSheet('color:#779341')
+        owner_projet_lbl.setStyleSheet(f'color:{LogInGUI.PRIMARY_COLOR}')
                 
         signin_lbl = QLabel('Sign in',self)
         signin_lbl.setFont(QFont("Tahoma",50))
@@ -44,7 +50,7 @@ class LogInGUI(QWidget):
         self.user_or_email_entry.resize(451,57)
         self.user_or_email_entry.move(44, 345)
         self.user_or_email_entry.setFont(QFont("Tahoma",12))
-        self.user_or_email_entry.setStyleSheet("padding:10px 20px;border-radius:4px;background-color:#FFFFFF;border: 1px solid #4285F4")
+        self.user_or_email_entry.setStyleSheet(f"padding:10px 20px;border-radius:4px;background-color:#FFFFFF;border: 1px solid {LogInGUI.BORDER_COLOR}")
         self.user_or_email_entry.setPlaceholderText("Username or email address")
         
         # -------- Password field:
@@ -56,21 +62,31 @@ class LogInGUI(QWidget):
         self.password_entry.resize(451,57)
         self.password_entry.move(44, 475)
         self.password_entry.setFont(QFont("Tahoma",12))
-        self.password_entry.setStyleSheet("padding:10px 20px;border-radius:4px;background-color:#FFFFFF;border: 1px solid #4285F4")
+        self.password_entry.setStyleSheet(f"padding:10px 20px;border-radius:4px;background-color:#FFFFFF;border: 1px solid {LogInGUI.BORDER_COLOR}")
         self.password_entry.setPlaceholderText("Password")
         
         # ------ Forget password: btn
         forget_btn = QPushButton("forget PASSWORD",self)
         forget_btn.setFont(QFont("Tahoma",9))
         forget_btn.move(370, 544)
-        forget_btn.setStyleSheet("background-color:transparent;color:#4285F4")
+        forget_btn.setStyleSheet(f"background-color:transparent;color:{LogInGUI.SECONDARY_COLOR}")
         
         # Sign in button
         signin_btn = QPushButton("Sign in",self)
         signin_btn.setFont(QFont("Tahoma",10))
         signin_btn.move(259, 608)
         signin_btn.resize(236, 54)
-        signin_btn.setStyleSheet("background-color: #779341;color:#fff;border-radius:4px")
+        signin_btn.setStyleSheet(f"background-color:{LogInGUI.PRIMARY_COLOR};color:#fff;border-radius:4px")
+        
+        # No account create one 
+        no_account_lbl = QLabel("No Account ?",self)
+        no_account_lbl.move(410, 50)
+        no_account_lbl.setFont(QFont("Tahoma",9))
+        # Create account btn
+        signup_btn = QPushButton("Sign up",self)
+        signup_btn.move(410, 70)
+        signup_btn.setFont(QFont("Tahoma",9))
+        signup_btn.setStyleSheet(f"background-color:transparent;color:{LogInGUI.PRIMARY_COLOR}")
     
         
 
